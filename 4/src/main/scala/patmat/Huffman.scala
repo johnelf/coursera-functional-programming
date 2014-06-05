752d32ca41487ff06c201e3c6e45f3ee74e03ee6
Zhang Jinwen <jwzhang@thoughtworks.com>
2014-06-05 17:31:25 +0800

    take a look at it

--- 4/src/main/scala/patmat/Huffman.scala
+++ 4/src/main/scala/patmat/Huffman.scala
@@ -134,16 +134,32 @@ object Huffman {
    * If `trees` is a list of less than two elements, that list should be returned
    * unchanged.
    */
-  def combine(trees: List[CodeTree]): List[CodeTree] = {
-    def sort(ele: Fork, list: List[CodeTree]): List[CodeTree] = list match {
-      case Nil => List(ele)
-      case (head: Leaf) :: tail =>
-        if (ele.weight < head.weight) ele :: head :: tail
-        else head :: sort(ele, tail)
+  def combine(trees: List[CodeTree]): List[CodeTree] =
+    if(trees.size < 2) trees
+    else (fork(trees.head, trees.tail.head) :: trees.drop(2))
+      .sortWith{
+      case (Leaf(_,weight1), Leaf(_,weight2)) => weight1 < weight2
+      case (Leaf(_,weight1), Fork(_,_,_,weight2)) => weight1 < weight2
+      case (Fork(_,_,_,weight1), Leaf(_,weight2)) => weight1 < weight2
+      case (Fork(_,_,_,weight1), Fork(_,_,_,weight2)) => weight1 < weight2
     }
-    if (trees.length < 3) trees
-    else combine(sort(makeCodeTree(trees(0), trees(1)), trees.drop(2)))
+
+  def fork(first: CodeTree, second: CodeTree): Fork = (first, second) match {
+    case (Leaf(char1, weight1), Leaf(char2, weight2)) => new Fork(first, second, List(char1, char2), weight1 + weight2)
+    case (Leaf(char1, weight1), Fork(_,_,chars2, weight2)) => new Fork(first, second, char1 :: chars2, weight1 + weight2)
+    case (Fork(_,_,chars1, weight1), Leaf(char2, weight2)) => new Fork(first, second, char2 :: chars1, weight1 + weight2)
+    case (Fork(_,_,chars1, weight1), Fork(_,_,chars2, weight2)) => new Fork(first, second, chars1 ::: chars2, weight1 + weight2)
   }
+
+//    def sort(ele: Fork, list: List[CodeTree]): List[CodeTree] = list match {
+//      case Nil => List(ele)
+//      case (head: Leaf) :: tail =>
+//        if (ele.weight < head.weight) ele :: head :: tail
+//        else head :: sort(ele, tail)
+//    }
+//    if (trees.length < 3) trees
+//    else combine(sort(makeCodeTree(trees(0), trees(1)), trees.drop(2)))
+//  }
 
   /**
    * This function will be called in the following way:
@@ -162,8 +178,8 @@ object Huffman {
    *    the example invocation. Also define the return type of the `until` function.
    *  - try to find sensible parameter names for `xxx`, `yyy` and `zzz`.
    */
-  def until(isSingleCodeTree: List[CodeTree] => Boolean, combine: List[CodeTree] => List[CodeTree])(trees: List[CodeTree]): List[CodeTree] = {
-    if (isSingleCodeTree(trees)) trees
+  def until(isSingleCodeTree: List[CodeTree] => Boolean, combine: List[CodeTree] => List[CodeTree])(trees: List[CodeTree]): CodeTree = {
+    if (isSingleCodeTree(trees)) trees.head
     else until(isSingleCodeTree, combine)(combine(trees))
   }
 
@@ -174,10 +190,9 @@ object Huffman {
    * frequencies from that text and creates a code tree based on them.
    */
   def createCodeTree(chars: List[Char]): CodeTree = {
-    if (chars.isEmpty) throw new NoSuchElementException
-    val list = combine(makeOrderedLeafList(times(chars)))
-    until(singleton, combine)(list)(0)
+    until(singleton, combine)(makeOrderedLeafList(times(chars)))
   }
+
 
   // Part 3: Decoding
 
